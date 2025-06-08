2e941f45c669cae002496962dc5a8732d725fa1b
bryanmwangindungu <bryanmwangi52@gmail.com>
2025-06-08 19:55:55 +0300

    4th commit

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -13,7 +13,7 @@ def calculate_future_date(days):
 
 def main():
     # Display current date and time
-    print("Current date and time:", displaycurrentdatetime())
+    print("Current date and time:",displaycurrentdatetime())
     
     try:
         # Get number of days to add from user
